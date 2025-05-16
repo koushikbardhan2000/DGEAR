9748adae468d1030c265cbbe1881d270ebc4b1c9
koushikbardhan2000 <koushikbardhan2000@gmail.com>
2025-05-16 20:20:56 +0530

    Backend integration

--- microarray.R
+++ microarray.R
@@ -189,6 +189,7 @@ dev.off()
 
 # gene list dataframe for STRINGdb
 gene_list <- data.frame(genes = row.names(ensembl_significant)[1:min(50, nrow(ensembl_significant))])
+# [1:min(50, nrow(ensembl_significant))] only build upto 50 genes ppi network
 # Initialize STRINGdb and map genes
 string_db <- STRINGdb$new(version="11.5", species=9606, score_threshold=400, input_directory="")
 mapped_data <- string_db$map(gene_list, "genes", removeUnmappedRows = TRUE)
