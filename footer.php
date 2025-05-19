8fef6011266010182d26c4b43a7fa20b74b17cba
koushikbardhan2000 <koushikbardhan2000@gmail.com>
2025-05-19 13:04:33 +0530

    Enable dynamic last updated date

--- footer.php
+++ footer.php
@@ -1,7 +1,21 @@
 <footer>
   <a href="https://www.nbu.ac.in/"><img class="logo-nbu" src="img/NBUlogo.png" alt="University of North Bengal Logo" width="24" height="24"></a>
   
-  <p>&copy;2024-2025 DGEAR-Web All rights reserved by Computational System Biology Lab | Department of Bioinformatics.<br>Last updated on: 19.05.2025</p>
+<p>
+  &copy;2024-2025 DGEAR-Web All rights reserved by Computational System Biology Lab | Department of Bioinformatics.<br>
+  Last updated on: <span id="lastUpdated"></span>
+</p>
+
+<script>
+  fetch('meta.json')
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById('lastUpdated').textContent = data.lastUpdated;
+    })
+    .catch(err => {
+      console.error("Failed to load update date:", err);
+    });
+</script>
   
   <a href="https://www.linkedin.com/in/koushik-bardhan-459895225/"><img class="logo-Lin" src="img/inlogo.jpg" alt="Koushik Bardhan linkedin logo" width="24" height="24"></a>
 </footer>
